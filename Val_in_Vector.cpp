306051d32d42f6e6b5dc4816c4f797c5767dd706
maguina-sj <jomaguina90@gmail.com>
2023-11-02 13:28:57 -0700

    sum of vector vals
    for loop to sum if conditions are met

--- Val_in_Vector.cpp
+++ Val_in_Vector.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 using namespace std;
 
+// Assign numMatches with the number of elements in userValues that equal matchValue. userValues has NUM_VALS elements. Ex: If userValues is {2, 2, 1, 2} and matchValue is 2 , then numMatches should be 3.
+
+Your code will be tested with the following values : *matchValue : 2, userValues : {2, 2, 1, 2}(as in the example program above)*matchValue : 0, userValues : {0, 0, 0, 0} * matchValue : 50, userValues: { 10, 20, 30, 40 }
 int main()
 {
   const int NUM_VALS = 4;
@@ -29,7 +32,7 @@ int main()
 
   return 0;
 }
-
+// Write a for loop to populate vector userGuesses with NUM_GUESSES integers. Read integers using cin. Ex: If NUM_GUESSES is 3 and user enters 9 5 2, then userGuesses is {9, 5, 2}.
 int main()
 {
   const int NUM_GUESSES = 3;
@@ -49,5 +52,31 @@ int main()
     cout << userGuesses.at(i) << " ";
   }
 
+  return 0;
+}
+
+// Vector testGrades contains NUM_VALS test scores. Write a for loop that sets sumExtra to the total extra credit received. Full credit is 100, so anything over 100 is extra credit. Ex: If testGrades = {101, 83, 107, 90}, then sumExtra = 8, because 1 + 0 + 7 + 0 is 8.
+int main()
+{
+  const int NUM_VALS = 4;
+  vector<int> testGrades(NUM_VALS);
+  unsigned int i;
+  int sumExtra = -9999; // Assign sumExtra with 0 before your for loop
+
+  for (i = 0; i < testGrades.size(); ++i)
+  {
+    cin >> testGrades.at(i);
+  }
+  sumExtra = 0;
+  for (i = 0; i < testGrades.size(); ++i)
+  {
+    if (testGrades.at(i) > 100)
+    {
+      sumExtra += (testGrades.at(i) - 100);
+    }
+  }
+  /* Your solution goes here  */
+
+  cout << "sumExtra: " << sumExtra << endl;
   return 0;
 }
